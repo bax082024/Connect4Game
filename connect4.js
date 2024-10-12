463179041babe2ea50b918585ab52cc215b7b6b2
bax082024 <bax082024@gmail.com>
2024-10-12 17:06:39 +0200

    finished createboard

--- connect4.js
+++ connect4.js
@@ -7,4 +7,17 @@ const createBoard = () => {
   const boardDiv = document.getElementById('board');
   boardDiv.innerHTML = '';
   board = Array(rows).fill(null).map(() => Array(cols).fill(null));
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.dataset.row = row;
+      cell.dataset.col = col;
+      cell-addEventListener('click', handleClick);
+      boardDiv.appendChild(cell);
+    }
+
+  }
 }
+
